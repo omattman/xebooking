072aceb2dfc0003725445aa2df4cf654a1db2c90
omattman <matthiashansen16@gmail.com>
2017-09-26 12:13:47 +0200

    added dynamic home URLS for easier links

--- header.php
+++ header.php
@@ -53,6 +53,8 @@
 
 	$et_top_info_defined = $et_secondary_nav_items->top_info_defined;
 
+	$homeurl = esc_url( home_url( '/' ) );
+
 	$et_slide_header = 'slide' === et_get_option( 'header_style', 'left' ) || 'fullscreen' === et_get_option( 'header_style', 'left' ) ? true : false;
 ?>
             <header class="nav__header">
@@ -80,134 +82,134 @@
 		                                <a href="/priser"><span>🎉 </span>Find din samlede pakkeløsning her<span class="flip-horizontal"> 🎉</span></a>
 		                            </li>
 		                            <li class="nav__menu-links">
-		                                <a class="t__h5 c__blue--xlg c__blue--lg c__grey--md c__grey--sm f__left" href="/musik/">Musik</a>
+		                                <a class="t__h5 c__blue--xlg c__blue--lg c__grey--md c__grey--sm f__left" href="<?php echo $homeurl ?>musik/">Musik</a>
 		                                <ul class="js-hidden">
 		                                    <li class="js-go-back">
 		                                        <a href="#0">Tilbage</a>
 		                                    </li>
 											<li class="see-all">
-		                                        <a href="/kategorier/koncertnavne/">Se al musik</a>
+		                                        <a href="<?php echo $homeurl ?>musik/koncertnavne">Se al musik</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="musik/koncertnavne/">Koncertnavne</a>
+		                                        <a href="<?php echo $homeurl ?>musik/koncertnavne">Koncertnavne</a>
 		                                    </li>
 											<li>
-		                                        <a href="musik/bryllupsbands/">Bryllupsbands</a>
+		                                        <a href="<?php echo $homeurl ?>musik/bryllupsbands">Bryllupsbands</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="musik/bands/">Bands</a>
+		                                        <a href="<?php echo $homeurl ?>musik/bands">Bands</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="musik/djs/">DJs</a>
+		                                        <a href="<?php echo $homeurl ?>musik/djs">DJs</a>
 		                                    </li>
 											<li>
-		                                        <a href="musik/solister/">Solister</a>
+		                                        <a href="<?php echo $homeurl ?>musik/solister">Solister</a>
 		                                    </li>
 											<li>
-		                                        <a href="musik/receptions-musik/">Receptions musik</a>
+		                                        <a href="<?php echo $homeurl ?>musik/receptions-musik">Receptions musik</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="musik/jazzlounge/">Jazz/Lounge</a>
+		                                        <a href="<?php echo $homeurl ?>musik/jazzlounge">Jazz/Lounge</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="musik/musik">Se alle kategorier</a>
+		                                        <a href="<?php echo $homeurl ?>musik/musik">Se alle kategorier</a>
 		                                    </li>
 		                                </ul>
 		                            </li>
 		                            <li class="nav__menu-links">
-		                                <a class="t__h5 c__blue--xlg c__blue--lg c__grey--md c__grey--sm f__left" href="/kategorier/koncertnavne/">Underholdning</a>
+		                                <a class="t__h5 c__blue--xlg c__blue--lg c__grey--md c__grey--sm f__left" href="<?php echo $homeurl ?>underholdning/">Underholdning</a>
 		                                <ul class="js-hidden">
 		                                    <li class="js-go-back">
 		                                        <a href="#0">Tilbage</a>
 		                                    </li>
 											<li class="see-all">
-		                                        <a href="/underholdning/">Se al underholdning</a>
+												<a href="<?php echo $homeurl ?>underholdning">Se al underholdning</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/underholdning//stand-up/">Stand-up</a>
+		                                        <a href="<?php echo $homeurl ?>underholdning/stand-up">Stand-up</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/underholdning//foredragsholder/">Foredragsholder</a>
+		                                        <a href="<?php echo $homeurl ?>underholdning/foredragsholder">Foredragsholder</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/underholdning//tryllekunstner/">tryllekunstner</a>
+		                                        <a href="<?php echo $homeurl ?>underholdning/tryllekunstner">Tryllekunstner</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/underholdning//happening/">Happening / Overraskelse</a>
+		                                        <a href="<?php echo $homeurl ?>underholdning/happening">Happening / Overraskelse</a>
 		                                    </li>
 											<li>
-		                                        <a href="/underholdning//underholdning-engelsk/">Underholdning (ENG)</a>
+		                                        <a href="<?php echo $homeurl ?>underholdning/underholdning-engelsk">Underholdning (ENG)</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/underholdning/">Se alle kategorier</a>
+		                                        <a href="<?php echo $homeurl ?>underholdning">Se alle kategorier</a>
 		                                    </li>
 		                                </ul>
 		                            </li>
 		                            <li class="nav__menu-links">
-		                                <a class="t__h5 c__blue--xlg c__blue--lg c__grey--md c__grey--sm f__left" href="/konferenciers-vaerter/">Konferenciers / Værter</a>
+		                                <a class="t__h5 c__blue--xlg c__blue--lg c__grey--md c__grey--sm f__left" href="<?php echo $homeurl ?>konferenciers-vaerter/">Konferenciers / Værter</a>
 		                                <ul class="js-hidden">
 		                                    <li class="js-go-back">
 		                                        <a href="#0">Tilbage</a>
 		                                    </li>
 		                                    <li class="dropdown__desc">
-		                                        <a href="/konferencier/">Konferencier</a>
+		                                        <a href="<?php echo $homeurl ?>konferencier">Konferencier</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/ordstyrer/">Ordstyrer</a>
+		                                        <a href="<?php echo $homeurl ?>ordstyrer">Ordstyrer</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/konferencier/">Se alle kategorier</a>
+		                                        <a href="<?php echo $homeurl ?>konferencier">Se alle kategorier</a>
 		                                    </li>
 		                                </ul>
 		                            </li>
 									<li class="nav__menu-links">
-		                                <a class="t__h5 c__blue u__hidden--md u__hidden--sm f__left" href="/artister/">Artister</a>
+		                                <a class="t__h5 c__blue u__hidden--md u__hidden--sm f__left" href="<?php echo $homeurl ?>artister/">Artister</a>
 		                                <ul class="js-hidden">
 		                                    <li class="js-go-back">
 		                                        <a href="#0">Tilbage</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/kategorier/koncertnavne/">Prisklasse 1</a>
+		                                        <a href="<?php echo $homeurl ?>artister/prisklasse-1">Prisklasse 1</a>
 		                                    </li>
 		                                    <li class="dropdown__desc">
-		                                        <a href="/kategorier/sangere-solister/">Prisklasse 2</a>
+		                                        <a href="<?php echo $homeurl ?>artister/prisklasse-2">Prisklasse 2</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/kategorier/sangere-solister/">Prisklasse 3</a>
+		                                        <a href="<?php echo $homeurl ?>artister/prisklasse-3">Prisklasse 3</a>
 		                                    </li>
 											<li>
-		                                        <a href="/kategorier/sangere-solister/">Artister i særklasse</a>
+		                                        <a href="<?php echo $homeurl ?>artister-i-særklasse">Artister i særklasse</a>
 		                                    </li>
 		                                    <li>
-		                                        <a href="/artister/">Se alle artister</a>
+		                                        <a href="<?php echo $homeurl ?>artister/">Se alle artister</a>
 		                                    </li>
 		                                </ul>
 		                            </li>
 									<li class="bg__blue c__white see-all u__hidden--xlg u__hidden--lg">
-										<a href="/priser">Få en samlet pakkeløsning</a>
+										<a href="<?php echo $homeurl ?>priser">Få en samlet pakkeløsning</a>
 									</li>
 		                        </ul>
 		                        </li>
 		                        <li>
-		                            <a href="/priser">Priser</a>
+		                            <a href="<?php echo $homeurl ?>priser">Priser</a>
 		                        </li>
 		                        <li>
-		                            <a href="/referencer">Referencer</a>
+		                            <a href="<?php echo $homeurl ?>referencer">Referencer</a>
 		                        </li>
 		                        <li>
-		                            <a href="/Nyheder">Nyheder</a>
+		                            <a href="<?php echo $homeurl ?>nyheder">Nyheder</a>
 		                        </li>
 		                        <li>
-		                            <a href="/Booking">Booking</a>
+		                            <a href="<?php echo esc_url( home_url( '/' ) ); ?>/Booking">Booking</a>
 		                        </li>
 		                        <li>
-		                            <a href="/om-xe">Om XE</a>
+		                            <a href="<?php echo $homeurl ?>om-xe">Om XE</a>
 		                        </li>
 		                        <li>
-		                            <a href="/Kontakt">Kontakt</a>
+		                            <a href="<?php echo $homeurl ?>kontakt">Kontakt</a>
 		                        </li>
 								<li class="bg__blue c__white see-all u__hidden--xlg u__hidden--lg">
-									<a href="/priser">Find pakkeløsning her</a>
+									<a href="<?php echo $homeurl ?>priser">Find pakkeløsning her</a>
 								</li>
 		                    </ul>
 		                </nav>
