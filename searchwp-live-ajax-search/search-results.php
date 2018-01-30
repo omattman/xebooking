0069cf798c17bbc199180f7dc327dc594bd6fe9e
omattman <matthiashansen16@gmail.com>
2018-01-30 09:08:23 +0100

    Ajax live search functionality to navigation

---
+++ searchwp-live-ajax-search/search-results.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Search results are contained within a div.searchwp-live-search-results
+ * which you can style accordingly as you would any other element on your site
+ *
+ * Some base styles are output in wp_footer that do nothing but position the
+ * results container and apply a default transition, you can disable that by
+ * adding the following to your theme's functions.php:
+ *
+ * add_filter( 'searchwp_live_search_base_styles', '__return_false' );
+ *
+ * There is a separate stylesheet that is also enqueued that applies the default
+ * results theme (the visual styles) but you can disable that too by adding
+ * the following to your theme's functions.php:
+ *
+ * wp_dequeue_style( 'searchwp-live-search' );
+ *
+ * You can use ~/searchwp-live-search/assets/styles/style.css as a guide to customize
+ */
+?>
+
+<?php if ( have_posts() ) : ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<?php $post_type = get_post_type_object( get_post_type() ); ?>
+		<div class="searchwp-live-search-result">	
+			<a class="searchwp-live-search-item" href="<?php echo esc_url( get_permalink() ); ?>">
+				<figure><?php echo get_the_post_thumbnail( $page->ID, array(75, 75) ); ?></figure>
+				<figcaption>
+					<p class="searchwp-live-search-artist-title c__blue"><?php the_title(); ?></p>
+					<p class="searchwp-live-search-artist-category"><?php echo strip_tags(get_the_term_list( get_the_ID(), 'project_category', '', ' | ' ) ); ?></p>
+				</figcaption>
+			</a>
+		</div>
+	<?php endwhile; ?>
+<?php else : ?>
+	<p class="searchwp-live-search-no-results">
+		<p class="f__center"><?php _ex( 'Desværre, vi kunne ikke finde nogen resultater.', 'swplas' ); ?></p>
+		<p class="f__center">Ring <a class="c__blue" href="tel:+457017025">+45 7021 7025</a> for professionel vejledning og booking.</p>
+	</p>
+<?php endif; ?>
