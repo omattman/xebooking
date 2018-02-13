9c256659c336aedd52e4b441d47223fcf681b5ab
omattman <matthiashansen16@gmail.com>
2018-02-13 12:58:45 +0100

    z index to footer and fix of gulp file error. Thusmore added template for artist pages

--- functions.php
+++ functions.php
@@ -83,6 +83,94 @@ function remove_metaboxes() {
 }
 add_action('admin_menu','remove_metaboxes');
 
+
+if ( ! function_exists( 'et_pb_register_posttypes' ) ) :
+    function et_pb_register_posttypes() {
+        $labels = array(
+            'name'               => esc_html__( 'Projects', 'et_builder' ),
+            'singular_name'      => esc_html__( 'Project', 'et_builder' ),
+            'add_new'            => esc_html__( 'Add New', 'et_builder' ),
+            'add_new_item'       => esc_html__( 'Add New Project', 'et_builder' ),
+            'edit_item'          => esc_html__( 'Edit Project', 'et_builder' ),
+            'new_item'           => esc_html__( 'New Project', 'et_builder' ),
+            'all_items'          => esc_html__( 'All Projects', 'et_builder' ),
+            'view_item'          => esc_html__( 'View Project', 'et_builder' ),
+            'search_items'       => esc_html__( 'Search Projects', 'et_builder' ),
+            'not_found'          => esc_html__( 'Nothing found', 'et_builder' ),
+            'not_found_in_trash' => esc_html__( 'Nothing found in Trash', 'et_builder' ),
+            'parent_item_colon'  => '',
+        );
+    
+        $args = array(
+            'labels'             => $labels,
+            'public'             => true,
+            'publicly_queryable' => true,
+            'show_ui'            => true,
+            'can_export'         => true,
+            'show_in_nav_menus'  => true,
+            'show_in_rest'       => true,
+            'query_var'          => true,
+            'has_archive'        => true,
+            'rewrite'            => apply_filters( 'et_project_posttype_rewrite_args', array(
+                'feeds'      => true,
+                'slug'       => 'project',
+                'with_front' => false,
+            ) ),
+            'capability_type'    => 'post',
+            'hierarchical'       => false,
+            'menu_position'      => null,
+            'supports'           => array( 'title', 'author', 'editor', 'thumbnail', 'excerpt', 'comments', 'revisions', 'custom-fields' ),
+        );
+    
+        register_post_type( 'project', apply_filters( 'et_project_posttype_args', $args ) );
+    
+        $labels = array(
+            'name'              => esc_html__( 'Project Categories', 'et_builder' ),
+            'singular_name'     => esc_html__( 'Project Category', 'et_builder' ),
+            'search_items'      => esc_html__( 'Search Categories', 'et_builder' ),
+            'all_items'         => esc_html__( 'All Categories', 'et_builder' ),
+            'parent_item'       => esc_html__( 'Parent Category', 'et_builder' ),
+            'parent_item_colon' => esc_html__( 'Parent Category:', 'et_builder' ),
+            'edit_item'         => esc_html__( 'Edit Category', 'et_builder' ),
+            'update_item'       => esc_html__( 'Update Category', 'et_builder' ),
+            'add_new_item'      => esc_html__( 'Add New Category', 'et_builder' ),
+            'new_item_name'     => esc_html__( 'New Category Name', 'et_builder' ),
+            'menu_name'         => esc_html__( 'Categories', 'et_builder' ),
+        );
+    
+        register_taxonomy( 'project_category', array( 'project' ), array(
+            'hierarchical'      => true,
+            'labels'            => $labels,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+        ) );
+    
+        $labels = array(
+            'name'              => esc_html__( 'Project Tags', 'et_builder' ),
+            'singular_name'     => esc_html__( 'Project Tag', 'et_builder' ),
+            'search_items'      => esc_html__( 'Search Tags', 'et_builder' ),
+            'all_items'         => esc_html__( 'All Tags', 'et_builder' ),
+            'parent_item'       => esc_html__( 'Parent Tag', 'et_builder' ),
+            'parent_item_colon' => esc_html__( 'Parent Tag:', 'et_builder' ),
+            'edit_item'         => esc_html__( 'Edit Tag', 'et_builder' ),
+            'update_item'       => esc_html__( 'Update Tag', 'et_builder' ),
+            'add_new_item'      => esc_html__( 'Add New Tag', 'et_builder' ),
+            'new_item_name'     => esc_html__( 'New Tag Name', 'et_builder' ),
+            'menu_name'         => esc_html__( 'Tags', 'et_builder' ),
+        );
+    
+        register_taxonomy( 'project_tag', array( 'project' ), array(
+            'hierarchical'      => false,
+            'labels'            => $labels,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'query_var'         => true,
+        ) );
+    }
+    endif;
+
+
 // Customize Wordpress Admin panel names
 function wd_admin_menu_rename() {
      global $menu;
@@ -182,9 +270,11 @@ function custom_project_path () {
         'slug' => 'book',
         'with_front' => false,
         'has_archive' => false,
+        'show_in_rest' => true
     );
 }
 add_filter( 'et_project_posttype_rewrite_args', 'custom_project_path' );
+
 
 
 function wpseo_remove_breadcrumb_link( $link_output , $link ){
@@ -299,5 +389,7 @@ function user_the_categories() {
     // echo the remaining categories, appending separator
     for ($i = 1; $i < count($cats); $i++) {echo ', ' . $cats[$i]->cat_name ;}
 }
+
+
 //END ENQUEUE PARENT ACTION
 ?>
