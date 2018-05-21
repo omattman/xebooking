7b0b303f8319b1965d4a40b5dcdd4c61945206e5
Matthias Hansen <matthiashansen16@gmail.com>
2018-05-21 12:19:28 +0200

    removed divi functionality from header and changed the setup of the navigation

--- header.php
+++ header.php
@@ -37,35 +37,20 @@
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KKXNQCX" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
 	<div class="js-nav-fixed">
-<?php
+	<?php
 	if ( is_page_template( 'page-template-blank.php' ) ) {
 		return;
 	}
-
-	$et_secondary_nav_items = et_divi_get_top_nav_items();
-
-	$et_phone_number = $et_secondary_nav_items->phone_number;
-
-	$et_email = $et_secondary_nav_items->email;
-
-	$et_contact_info_defined = $et_secondary_nav_items->contact_info_defined;
-
-	$show_header_social_icons = $et_secondary_nav_items->show_header_social_icons;
-
-	$et_secondary_nav = $et_secondary_nav_items->secondary_nav;
-
-	$et_top_info_defined = $et_secondary_nav_items->top_info_defined;
-
-	$et_slide_header = 'slide' === et_get_option( 'header_style', 'left' ) || 'fullscreen' === et_get_option( 'header_style', 'left' ) ? true : false;
-?>
+	?>
             <header class="nav__header">
-				<div class="nav__detail nav__detail-wrapper u__hidden--md u__hidden--sm">
+				<div class="nav__detail nav__detail-wrapper u__hidden--md f__center--sm">
 					<div class="container narrow">
 						<div class="g__flex nav__padding">
 							<div class="nav__detail-wrapper">
-								<span class="nav__detail-text"><a href="//www.xe.dk/kontakt/"><font color="#FF6900">Gratis</font> professionel rådgivning</a></span>
-								<span class="nav__detail-text"><a href="tel:+4570217025">Ring direkte <font color="#FF6900">+45 7021 7025</font></a></span>
-								<span class="nav__detail-text"><a href="//www.xe.dk/kontakt/"><font color="#FF6900">+25 års</font> erfaring og <font color="#FF6900">+100</font> anbefalinger</a></span>
+								<span class="nav__detail-text u__hidden--sm"><a href="//www.xe.dk/artister/">Book Danmarks bedste <font color="#FF6900">artister</font></a></span>
+								<span class="nav__detail-text u__hidden--sm"><a href="tel:+4570217025">Ring <font color="#FF6900">+45 7021 7025</font> for booking</a></span>
+								<span class="u__full f__bold u__hidden--xlg u__hidden--lg u__hidden--md"><a href="tel:+4570217025">Få hjælp og book nu <font color="#FF6900">+45 7021 7025</font></a></span>
+								<span class="nav__detail-text u__hidden--sm"><a href="//www.xe.dk/referencer/"><font color="#FF6900">+25 års</font> erfaring og <font color="#FF6900">+180</font> anbefalinger</a></span>
 							</div>
 						</div>
 					</div>
@@ -78,17 +63,28 @@
 							: $template_directory_uri . '/images/logo.png';
 						?>
 						<a class="nav__menu-logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-							<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" id="logo" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
+							<img src="<?php echo esc_attr( $logo ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" data-height-percentage="<?php echo esc_attr( et_get_option( 'logo_height', '54' ) ); ?>" />
 						</a>
 						<nav class="nav__menu-mobile"><ul id="nav__primary" class="nav__primary is-fixed">
-							<li class="u__hidden--xlg u__hidden--lg">
+							<div class="nav__close-button">
+								<span class="nav__close-icon">
+									<svg id="icon_svg-close-white-small" viewBox="0 0 24 24" width="100%" height="100%">
+										<path d="M13.099 12.038l3.977 3.978-1.06 1.06-3.978-3.977-3.977 3.977L7 16.016l3.977-3.978L7 8.061 8.06 7l3.978 3.977L16.016 7l1.06 1.06-3.977 3.978z" fill="#FFF" fill-rule="evenodd"></path>
+									</svg>
+								</span>
+							</div>
+							<div class="nav__image"></div>
+							<li class="nav__home u__hidden--xlg u__hidden--lg">
 								<a href="<?php echo esc_url( home_url( '/' ) ); ?>">Forside</a>
 							</li>
 							<li class="nav__menu-links">
-								<a class="nav__menu-category" href="#0">Musik</a>
+								<a class="nav__menu-category f__up" href="//www.xe.dk/musik/">
+									<span>Musik</span>
+									<div class="nav__menu-category-divider"></div>
+								</a>
 								<ul class="nav__secondary js-hidden">
 									<li class="js-go-back">
-										<a href="#0">Tilbage</a>
+										<a href="javascript:void(0);">Tilbage</a>
 									</li>
 									<a class="u__hidden--xlg u__hidden--lg" href="//www.xe.dk/musik/">Se al musik</a>
 									<a href="//www.xe.dk/musik/djs/">DJs</a>
@@ -98,10 +94,13 @@
 									<a href="//www.xe.dk/musik/musik-til-reception/">Receptionsmusik</a>
 									<a href="//www.xe.dk/musik/">Se alle kategorier</a>
 								</ul>
-								<a class="nav__menu-category" href="#0">Underholdning</a>
+								<a class="nav__menu-category f__up" href="//www.xe.dk/underholdning/">
+									<span>Underholdning</span>
+									<div class="nav__menu-category-divider"></div>
+								</a>
 								<ul class="nav__secondary js-hidden">
 									<li class="js-go-back">
-										<a href="#0">Tilbage</a>
+										<a href="javascript:void(0);">Tilbage</a>
 									</li>
 									<a class="u__hidden--xlg u__hidden--lg" href="//www.xe.dk/underholdning/">Se al underholdning</a>
 									<a href="//www.xe.dk/underholdning/stand-up/">Stand-up</a>
@@ -109,14 +108,16 @@
 									<a href="//www.xe.dk/underholdning/tryllekunstner/">Tryllekunstner</a>
 									<a href="//www.xe.dk/underholdning/entertainer/">Entertainer</a>
 									<a href="//www.xe.dk/underholdning/entertainer-engelsk/">Entertainer (ENG)</a>
-									<a href="//www.xe.dk/underholdning/">S	e alle kategorier</a>
+									<a href="//www.xe.dk/underholdning/">Se alle kategorier</a>
 								</ul>
-								<a class="nav__menu-category" href="#0">Konferenciers</a>
+								<a class="nav__menu-category f__up" href="//www.xe.dk/vaerter/">
+									<span>Værter</span>
+								</a>
 								<ul class="nav__secondary js-hidden">
 									<li class="js-go-back">
-										<a href="#0">Tilbage</a>
+										<a href="javascript:void(0);">Tilbage</a>
 									</li>
-									<a class="u__hidden--xlg u__hidden--lg" href="//www.xe.dk/konferenciers-vaerter/">Se alle værter</a>
+									<a class="u__hidden--xlg u__hidden--lg" href="//www.xe.dk/vaerter/">Se alle værter</a>
 									<a href="//www.xe.dk/konferenciers-vaerter/konferencier/">Konferencier</a>
 									<a href="//www.xe.dk/konferenciers-vaerter/ordstyrer/">Ordstyrer</a>
 									<a href="//www.xe.dk/konferenciers-vaerter/">Se alle kategorier</a>
@@ -130,8 +131,8 @@
 								<li class="u__hidden--xlg u__hidden--lg">
 										<a href="//www.xe.dk/nyheder/">Nyheder</a>
 								</li>
-								<li class="u__hidden--xlg u__hidden--lg">
-										<a href="//www.xe.dk/forespoergsel/">Booking</a>
+								<li class="nav__link-booking u__hidden--xlg u__hidden--lg">
+										<a class="c__orange" href="//www.xe.dk/forespoergsel/">Booking</a>
 								</li>
 								<li class="u__hidden--xlg u__hidden--lg">
 										<a href="//www.xe.dk/om-xe/">Om XE</a>
@@ -139,44 +140,43 @@
 								<li class="u__hidden--xlg u__hidden--lg">
 										<a href="//www.xe.dk/kontakt/">Kontakt</a>
 								</li>
-								<li class="bg__blue c__white see-all u__hidden--xlg u__hidden--lg">
-									<a href="//www.xe.dk/priser/">Find pakkeløsning her</a>
-								</li>
-							</ul>
+							</li>
 						</nav>
-						<ul class="nav__menu-buttons">
+						<div class="nav__menu-buttons">
 							<li>
 								<a class="nav__search-trigger u__hidden--xlg u__hidden--lg" title="Søg blandt vores artister">
 									<span></span>
 								</a>
 							</li>
 							<li>
-								<a class="nav__menu-trigger" title="Åben menu"><span></span</a>
+								<a class="nav__menu-trigger" title="Åben menu">
+									<span></span>
+								</a>
 							</li>
-						</ul>
+						</div>
 					</div>
-					<div class="nav__bottom u__hidden--md u__hidden--sm">
-						<ul class="nav__bottom-category u__reset f__left">
+					<div class="nav__bottom u__flex u__hidden--md u__hidden--sm">
+						<ul class="nav__bottom-category u__flex u__reset f__right">
 							<li class="nav__bottom-category-element u__inline-block">
-								<a href="//www.xe.dk">Priser</a>
+								<a href="//www.xe.dk/priser/">Priser</a>
 							</li>
 							<li class="nav__bottom-category-element u__inline-block">
-								<a href="//www.xe.dk">Referencer</a>
+								<a href="//www.xe.dk/referencer/">Referencer</a>
 							</li>
 							<li class="nav__bottom-category-element u__inline-block">
-								<a href="//www.xe.dk">Nyheder</a>
+								<a href="//www.xe.dk/nyheder/">Nyheder</a>
 							</li>
 							<li class="nav__bottom-category-element u__inline-block">
-								<a href="//www.xe.dk">Om XE</a>
+								<a href="//www.xe.dk/om-xe/">Om XE</a>
 							</li>
 							<li class="nav__bottom-category-element u__inline-block">
-								<a href="//www.xe.dk">Kontakt</a>
+								<a href="//www.xe.dk/kontakt/">Kontakt</a>
 							</li>
 							<li class="nav__bottom-category-element u__inline-block">
-								<a class="c__orange" href="//www.xe.dk">Booking</a>
+								<a class="c__orange" href="//www.xe.dk/forespoergsel/">Booking</a>
 							</li>
 						</ul>
-						<div class="nav__bottom-search">
+						<div class="nav__bottom-search u__flex">
 							<form class="nav__bottom-search-form" role="search" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 							<span class="nav__bottom-search-icon"><svg width="26" height="26" viewBox="0 0 26 26"><<svg id="icon_svg-search-semi-bold" viewBox="0 0 26 26" width="100%" height="100%"><path d="M16.979 17.314a6.181 6.181 0 0 1-4.474 1.506c-3.508-.23-6.198-3.314-6.024-6.888.175-3.583 3.163-6.312 6.68-6.081 3.506.23 6.196 3.314 6.022 6.887a6.484 6.484 0 0 1-1.194 3.466l4.199 3.703-.992 1.125-4.217-3.718zm-4.376.009c-2.683-.176-4.759-2.554-4.624-5.318.135-2.753 2.411-4.832 5.083-4.658 2.682.177 4.757 2.555 4.623 5.318-.134 2.754-2.41 4.833-5.082 4.658z" fill="currentColor" fill-rule="evenodd"></path></svg></svg></span>
 							<?php
